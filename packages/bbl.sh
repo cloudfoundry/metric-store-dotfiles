5122fb09d31c6ac93e78a742fc0697cba33641d7
Todd Persen <tpersen@pivotal.io>
2018-07-13 13:56:54 -0700

    Bump bbl to v6.8.3

--- packages/bbl.sh
+++ packages/bbl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=6.8.1
+VERSION=6.8.3
 
 wget https://github.com/cloudfoundry/bosh-bootloader/releases/download/v$VERSION/bbl-v$VERSION\_linux_x86-64
 sudo mv bbl-v$VERSION\_linux_x86-64 /usr/local/bin/bbl
