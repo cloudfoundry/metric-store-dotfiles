4e9129b3fdc234d20bc2d794a56bf15f39cd043e
Todd Persen <tpersen@pivotal.io>
2018-07-16 13:31:13 -0700

    Clean up apt install commands and profile links

--- install.sh
+++ install.sh
@@ -3,11 +3,13 @@
 set -ex
 
 sudo apt update
-sudo apt install -y automake build-essential pkg-config
-sudo apt-get install neovim git curl htop silversearcher-ag openssh-server
-sudo apt-get install fonts-inconsolata
+sudo apt install -y automake build-essential pkg-config cmake
+sudo apt install -y neovim tmux git curl htop silversearcher-ag openssh-server
+sudo apt install -y fonts-inconsolata
 
 sudo fc-cache -fv
+
+./clone-repos.sh
 
 PACKAGES=`ls -d packages/*.sh`
 for package in $PACKAGES
@@ -23,6 +25,7 @@ ln -sf $SRC/gitconfig $HOME/.gitconfig
 ln -sf $SRC/tmux.conf $HOME/.tmux.conf
 ln -sf $SRC/bashrc $HOME/.bashrc
 ln -sf $SRC/bash_profile $HOME/.bash_profile
+ln -sf $SRC/profile $HOME/.profile
 ln -sf $SRC/gemrc $HOME/.gemrc
 ln -sf $SRC/aliases $HOME/.aliases
 ln -sf $SRC/init.vim $HOME/.config/nvim/init.vim
