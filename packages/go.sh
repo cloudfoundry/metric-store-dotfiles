bfbb3f951edb93e1a99f698d8a7a103b30f7202f
Todd Persen <tpersen@pivotal.io>
2018-10-17 13:53:44 -0600

    Use go1.11, fix upgrade path

--- packages/go.sh
+++ packages/go.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=1.10.3
+VERSION=1.11.1
 
 if [[ `go version` = "go version go$VERSION "* ]]; then
     echo "Looks like go $VERSION is already installed, skipping..."
@@ -10,5 +10,5 @@ fi
 wget https://dl.google.com/go/go$VERSION.linux-amd64.tar.gz
 tar xzvf go$VERSION.linux-amd64.tar.gz
 sudo mv go /usr/local/go-$VERSION
-sudo ln -sf /usr/local/go-$VERSION /usr/local/go
+sudo ln -sf --no-dereference /usr/local/go-$VERSION /usr/local/go
 rm go$VERSION.linux-amd64.tar.gz
