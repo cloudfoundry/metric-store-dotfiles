51c6c7100e7a1ce504e8034f77d2fb16265df000
Mark Gangl <mgangl@pivotal.io>
2018-10-29 12:50:55 -0600

    add version checking to fly

--- packages/fly-cli.sh
+++ packages/fly-cli.sh
@@ -2,6 +2,11 @@
 
 VERSION=3.14.1
 
+if [[ `fly --version` = "$VERSION" ]]; then
+    echo "Looks like fly $VERSION is already installed, skipping..."
+    exit
+fi
+
 wget https://github.com/concourse/concourse/releases/download/v$VERSION/fly_linux_amd64
 sudo mv fly_linux_amd64 /usr/local/bin/fly
 chmod +x /usr/local/bin/fly
