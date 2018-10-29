cd10fa4e228696329f518aca1669306282eb9d45
Mark Gangl <mgangl@pivotal.io>
2018-10-29 12:53:18 -0600

    add version checking to git-duet

--- packages/git-duet.sh
+++ packages/git-duet.sh
@@ -2,6 +2,11 @@
 
 VERSION=0.6.0
 
+if [[ `git-duet --version` == $VERSION* ]]; then
+    echo "Looks like git-duet $VERSION is already installed, skipping..."
+    exit
+fi
+
 mkdir -p ~/bin
 wget -O git-duet-$VERSION.tar.gz https://github.com/git-duet/git-duet/releases/download/$VERSION/linux_amd64.tar.gz
 tar xzvf git-duet-$VERSION.tar.gz -C ~/bin/
