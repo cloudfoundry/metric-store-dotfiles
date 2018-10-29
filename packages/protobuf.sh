2000bed1cc3e2ae5ef77702682191388db097de4
Mark Gangl <mgangl@pivotal.io>
2018-10-29 12:55:21 -0600

    add version checking to protobuf

--- packages/protobuf.sh
+++ packages/protobuf.sh
@@ -3,6 +3,11 @@
 VERSION=3.6.1
 FILE=protoc-$VERSION-linux-x86_64.zip
 
+if [[ `protoc --version` = "libprotoc $VERSION" ]]; then
+    echo "Looks like fly $VERSION is already installed, skipping..."
+    exit
+fi
+
 pushd $HOME/workspace
   mkdir protobuf
   pushd protobuf
