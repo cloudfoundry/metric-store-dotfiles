f2dbaba948a693721bd37e6b0c4021605bee65b1
Mark Gangl <mgangl@pivotal.io>
2018-11-26 16:39:47 -0700

    Update msq to output curl command
    Signed-off-by: Johanna Smith <josmith@pivotal.io>

--- support/global-funcs.sh
+++ support/global-funcs.sh
@@ -1,11 +1,17 @@
 msq() {
-    echo "Querying ${BOSH_ENV_NAME}"
+    echo "Querying ${BOSH_ENV_NAME} ..."
     stdout_tmpfile=$(mktemp)
 
-    stderr_output=$(curl -H "Authorization: $(cf oauth-token)" -sS -G https://metric-store.${BOSH_ENV_NAME}.cf-denver.com/api/v1/query --data-urlencode ${1} > ${stdout_tmpfile} 2>&1)
+    url="https://metric-store.${BOSH_ENV_NAME}.cf-denver.com/api/v1/query"
+    stderr_output=$(curl -H "Authorization: $(cf oauth-token)" -sS -G ${url} \
+        --data-urlencode query=${1} > ${stdout_tmpfile} 2>&1)
     curl_status=$?
 
     if [ $curl_status -eq 0 ]; then
+        echo
+        echo "curl -H \"Authorization: \$(cf oauth-token)\" -sS -G ${url}" \
+            "--data-urlencode 'query=${1}'"
+        echo
         cat ${stdout_tmpfile} | jq .
     else
         cat ${stdout_tmpfile}
