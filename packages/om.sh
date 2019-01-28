33aff4640773214cc8187fa9e02b9f10033adddd
Mark Gangl <mgangl@pivotal.io>
2019-01-28 10:09:17 -0700

    add required om dependency

---
+++ packages/om.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+. ~/workspace/log-cache-dotfiles/support/helpers.sh
+
+APP="om"
+VERSION=0.47.0
+FILE="om-linux"
+
+echo_installing
+
+if [[ `om --version` = "${VERSION}" ]]; then
+    echo_already_installed
+    exit
+fi
+
+wget https://github.com/pivotal-cf/om/releases/download/${VERSION}/${FILE}
+chmod +x $FILE
+sudo mv $FILE /usr/local/bin/om
+
+echo_installed
