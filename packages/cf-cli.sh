9e55f5c21f1ee15f0a9cfc9cc6493ff0c930522c
Mark Gangl <mgangl@pivotal.io>
2019-02-15 13:40:45 -0700

    update cf-cli installation to be idempotent

--- packages/cf-cli.sh
+++ packages/cf-cli.sh
@@ -3,12 +3,19 @@
 . ~/workspace/log-cache-dotfiles/support/helpers.sh
 
 APP="cf-cli"
+VERSION=6.42.0
 
-echo_header "Installing cf-cli"
+echo_installing
+
+if [[ `cf version` = "cf version $VERSION"* ]]; then
+    echo_already_installed
+    exit
+fi
 
 start_install
   wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | sudo apt-key add -
   echo "deb https://packages.cloudfoundry.org/debian stable main" | sudo tee /etc/apt/sources.list.d/cloudfoundry-cli.list
   sudo apt-get update
-  sudo apt-get install cf-cli
+  sudo apt-get remove -y cf-cli
+  sudo apt-get install -y cf-cli=$VERSION
 end_install
