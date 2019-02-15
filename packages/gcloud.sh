36a031644c64660f5ecc09dac7738253e8ed8c6c
Mark Gangl <mgangl@pivotal.io>
2019-02-15 11:41:24 -0700

    install gcloud

---
+++ packages/gcloud.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+. ~/workspace/log-cache-dotfiles/support/helpers.sh
+
+APP="Gcloud"
+VERSION=234.0.0
+
+echo_installing
+
+if [[ `gcloud --version | head -1` = "Google Cloud SDK $VERSION" ]]; then
+    echo_already_installed
+    exit
+fi
+
+wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-${VERSION}-linux-x86_64.tar.gz -O google-cloud-sdk.tar.gz
+tar xzvf google-cloud-sdk.tar.gz -C /usr/local/share
+/usr/local/share/google-cloud-sdk/install.sh
+ln -s /usr/local/share/google-cloud-sdk/bin/gcloud /usr/local/bin/gcloud
+ln -s /usr/local/share/google-cloud-sdk/bin/gsutil /usr/local/bin/gsutil
+rm google-cloud-sdk.tar.gz
+
+sudo chown pivotal: -R ~/.config/gcloud
+
+echo_installed
