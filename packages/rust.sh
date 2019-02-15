bbeafc608ab473708bdbd46df3fd866412048340
Mark Gangl <mgangl@pivotal.io>
2019-02-15 13:59:26 -0700

    update rust installation to be idempotent

--- packages/rust.sh
+++ packages/rust.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
-curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain stable -y
-source $HOME/.cargo/env
+. ~/workspace/log-cache-dotfiles/support/helpers.sh
+
+APP="rust"
+VERSION="latest"
+
+echo_installing
+
+start_install
+  if [[ -e $HOME/.cargo/bin/rustup ]]; then
+    rustup self update
+    rustup update
+  else
+    curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain stable -y
+    source $HOME/.cargo/env
+  fi
+end_install
