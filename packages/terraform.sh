a6c7a67f553ad17e0b511f24df4ce6ba6c9d31f3
Mark Gangl <mgangl@pivotal.io>
2019-02-15 11:41:11 -0700

    install terraform

---
+++ packages/terraform.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+. ~/workspace/log-cache-dotfiles/support/helpers.sh
+
+APP="terraform"
+VERSION=0.11.11
+
+echo_installing
+
+if [[ `terraform version` = "Terraform v$VERSION" ]]; then
+    echo_already_installed
+    exit
+fi
+
+wget https://releases.hashicorp.com/terraform/${VERSION}/terraform_${VERSION}_linux_amd64.zip
+unzip terraform_${VERSION}_linux_amd64.zip
+rm  terraform_${VERSION}_linux_amd64.zip
+sudo mv terraform /usr/local/bin
+sudo chmod +x /usr/local/bin/terraform
+
+echo_installed
