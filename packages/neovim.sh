ec58818a676d92ee3ae4dd62806eec6503c68bed
Todd Persen <tpersen@pivotal.io>
2019-03-08 15:58:52 -0700

    fix neovim
    Signed-off-by: Johanna Ratliff <josmith@pivotal.io>

--- packages/neovim.sh
+++ packages/neovim.sh
@@ -13,14 +13,15 @@ start_install
   sudo apt-get install libtool libtool-bin
 
   pushd $HOME/workspace
-    mkdir neovim
+    mkdir -p neovim
     pushd neovim
-      wget https://github.com/neovim/neovim/archive/v$VERSION.tar.gz
-      tar xf v$VERSION.tar.gz
-      cd neovim-$VERSION
-      make CMAKE_BUILD_TYPE=RelWithDebInfo
-      sudo make install
+      wget https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz
+      tar xf nvim-linux64.tar.gz
+      sudo mv nvim-linux64/bin/nvim /usr/local/bin/nvim
+      chmod +x /usr/local/bin/nvim
     popd
     rm -rf neovim
   popd
+
+  pip3 install --upgrade pynvim
 end_install
