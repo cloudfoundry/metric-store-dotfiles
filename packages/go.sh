e5bea779f8ef4b17a0e4f52b92005d87a0651a58
Mark Gangl <mgangl@pivotal.io>
2019-03-29 12:32:32 -0600

    bump Go: 1.12.1

--- packages/go.sh
+++ packages/go.sh
@@ -2,7 +2,7 @@
 
 . ~/workspace/log-cache-dotfiles/support/helpers.sh
 
-identify_package "go" 1.11.5
+identify_package "go" 1.12.1
 
 if [[ `go version` = "go version go$VERSION "* ]]; then
     echo_already_installed
