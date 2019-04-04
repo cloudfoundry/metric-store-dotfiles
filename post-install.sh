6fefd951f7af35bacb53b944a08dea5585a20822
Mark Gangl <mgangl@pivotal.io>
2019-04-04 15:11:24 -0600

    interpolate slack-ip cron job on post-install

--- post-install.sh
+++ post-install.sh
@@ -14,3 +14,24 @@ echo_footer "cf-uaac installed"
 echo_header "Installing yq"
 pip3 install yq
 echo_footer "yq installed"
+
+echo_header "Installing slack-ip cron job"
+  lpass status -q
+  if [ $? -ne 0 ]; then
+    while true; do
+      read -p "What is your lpass email? " lpass_email
+
+      lpass login $lpass_email
+      if [ $? -eq 0 ]; then
+        break
+      fi
+    done
+  fi
+
+  export SLACK_URL=$(lpass show slack-ci.yml --notes | grep slack_alert_webhook_url | cut -d' ' -f2-)
+  cron_file=/etc/cron.hourly/slack-ip
+
+  cat ~/workspace/log-cache-dotfiles/tools/slack-ip.sh | sed -r 's/^(.*)SLACK_URL_PLACEHOLDER(.*)$/echo "\1${SLACK_URL}\2"/e' > /tmp/slack-cron
+  sudo mv /tmp/slack-cron ${cron_file}
+  sudo chmod 0755 ${cron_file}
+echo_footer "slack-ip cron job installed"
