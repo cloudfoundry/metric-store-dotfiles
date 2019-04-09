a6a6c40d7ac5178a761e3f469608fe64caa001db
Mark Gangl <mgangl@pivotal.io>
2019-04-09 16:41:35 -0600

    bump alacritty: 0.3.0
    Signed-off-by: Todd Persen <tpersen@pivotal.io>

--- packages/alacritty.sh
+++ packages/alacritty.sh
@@ -2,7 +2,7 @@
 
 . ~/workspace/log-cache-dotfiles/support/helpers.sh
 
-identify_package "Alacritty" 0.2.9
+identify_package "Alacritty" 0.3.0
 
 /bin/ps -auxw | /bin/grep " alacritty" | grep -q -v grep
 is_alacritty_running=$?
@@ -18,9 +18,9 @@ if [[ `alacritty --version` = "alacritty $VERSION" ]]; then
 fi
 
 start_install
-  wget https://github.com/jwilm/alacritty/releases/download/v${VERSION}/Alacritty-v${VERSION}-x86_64.tar.gz
-  tar xzf Alacritty-v${VERSION}-x86_64.tar.gz
-  rm Alacritty-v${VERSION}-x86_64.tar.gz
+  wget https://github.com/jwilm/alacritty/releases/download/v${VERSION}/Alacritty-v${VERSION}-ubuntu_18_04-x86_64.tar.gz
+  tar xzf Alacritty-v${VERSION}-ubuntu_18_04-x86_64.tar.gz
+  rm Alacritty-v${VERSION}-ubuntu_18_04-x86_64.tar.gz
   sudo mv alacritty /usr/local/bin
   sudo chmod +x /usr/local/bin/alacritty
   sudo desktop-file-install /home/pivotal/workspace/log-cache-dotfiles/assets/alacritty.desktop
