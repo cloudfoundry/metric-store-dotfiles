ab0acc26523b84981436c45956b9fc68e5836d84
Mark Gangl <mgangl@pivotal.io>
2019-04-10 10:34:08 -0600

    bump concourse fly cli: 4.2.5

--- packages/fly-cli.sh
+++ packages/fly-cli.sh
@@ -2,7 +2,7 @@
 
 . ~/workspace/log-cache-dotfiles/support/helpers.sh
 
-identify_package "fly" 3.14.1
+identify_package "fly" 4.2.5
 
 if [[ `fly --version` = "$VERSION" ]]; then
     echo_already_installed
@@ -10,7 +10,7 @@ if [[ `fly --version` = "$VERSION" ]]; then
 fi
 
 start_install
-  wget https://github.com/concourse/concourse/releases/download/v$VERSION/fly_linux_amd64
+  wget https://github.com/concourse/fly/releases/download/v$VERSION/fly_linux_amd64
   sudo mv fly_linux_amd64 /usr/local/bin/fly
   chmod +x /usr/local/bin/fly
 end_install
