b768c652ed246ddb39f9c153735f69e6ac4b6a37
Mark Gangl <mgangl@pivotal.io>
2019-04-12 12:12:46 -0600

    run slack-ip every minute

--- templates/slack-ip.sh
+++ templates/slack-ip.sh
@@ -5,16 +5,20 @@ CHANNEL="log-cache-stdout"
 USERNAME=$(hostname)
 LASTIPFILE='/home/pivotal/.last_ip_address'
 
-MYIP=$(hostname -I | cut -d' ' -f1 | tr -d '[:space:]')
-LASTIP=$(cat ${LASTIPFILE})
+for ((n=0;n<60;n++)); do
+  MYIP=$(hostname -I | cut -d' ' -f1 | tr -d '[:space:]')
+  LASTIP=$(cat ${LASTIPFILE})
 
-if [[ ${MYIP} != ${LASTIP} ]]; then
-  echo "New IP Address = ${MYIP}"
-  echo ${MYIP} > ${LASTIPFILE}
-  MESSAGETEXT="${USERNAME} IP address has changed. It is now: $MYIP"
-  JSON="{\"channel\": \"$CHANNEL\", \"username\":\"$USERNAME\", \"icon_emoji\":\"house\", \"attachments\":[{\"color\":\"danger\" , \"text\": \"$MESSAGETEXT\"}]}"
+  if [[ ${MYIP} != ${LASTIP} ]]; then
+    echo "New IP Address = ${MYIP}"
+    echo ${MYIP} > ${LASTIPFILE}
+    MESSAGETEXT="${USERNAME} IP address has changed. It is now: $MYIP"
+    JSON="{\"channel\": \"$CHANNEL\", \"username\":\"$USERNAME\", \"icon_emoji\":\"house\", \"attachments\":[{\"color\":\"danger\" , \"text\": \"$MESSAGETEXT\"}]}"
 
-  curl -s -d "payload=$JSON" "$WEBHOOK_URL"
-else
-  echo "IP address (${MYIP}) has not changed."
-fi
+    curl -s -d "payload=$JSON" "$WEBHOOK_URL"
+  else
+    echo "IP address (${MYIP}) has not changed."
+  fi
+
+  sleep 60
+done
