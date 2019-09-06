76433e1b687bc83656dbb3a3d4909cc23c02be9f
Joe Gee <jgee@pivotal.io>
2019-09-06 09:48:50 -0600

    Add a function for querying the closed source metric store deployment

--- support/global-funcs.sh
+++ support/global-funcs.sh
@@ -18,6 +18,27 @@ msq() {
     fi
 }
 
+
+pmsq() {
+    echo "Querying ${BOSH_ENV_NAME} ..."
+    stdout_tmpfile=$(mktemp)
+
+    url="https://ms.${BOSH_ENV_NAME}.cf-denver.com/api/v1/query"
+    stderr_output=$(curl -k -H "Authorization: $(cf oauth-token)" -sS -G ${url} \
+        --data-urlencode query=${1} > ${stdout_tmpfile} 2>&1)
+    curl_status=$?
+
+    if [ $curl_status -eq 0 ]; then
+        echo
+        echo "curl -H \"Authorization: \$(cf oauth-token)\" -sS -G ${url}" \
+            "--data-urlencode 'query=${1}'"
+        echo
+        cat ${stdout_tmpfile} | jq .
+    else
+        cat ${stdout_tmpfile}
+    fi
+}
+
 tmux_get_window_name() {
     if [ -v TMUX ]; then
         tmux display-message -p "#W"
