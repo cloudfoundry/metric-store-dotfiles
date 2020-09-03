cfa9347000906de440eae8fe4b21112fb95e1e09
Mark Gangl <mgangl@pivotal.io>
2020-09-03 20:04:51 +0000

    bump golang version to 1.15.1

--- packages/go.sh
+++ packages/go.sh
@@ -2,7 +2,7 @@
 
 . ~/workspace/metric-store-dotfiles/support/helpers.sh
 
-identify_package "go" 1.13.8
+identify_package "go" 1.15.1
 
 if [[ `go version` = "go version go$VERSION "* ]]; then
     echo_already_installed
